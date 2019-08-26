46099328b484d515258ccb8be0a7a4e150ce32c2
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-26 13:19:13 -0400

    added explaination

--- two-to-one.js
+++ two-to-one.js
@@ -23,3 +23,11 @@ const longest = (sOne, sTwo) => {
 }
 
 console.log(`Final Function: `, longest(sOne, sTwo))
+
+/*
+BEST INTERESTING SOLUTIONS
+Set, which is a new data object introduced in ES6. Because Set only lets you store unique values. When you pass in an array, it will remove any duplicate values.
+He uses the set object to remove any duplicates and combines both strings. at the same time he is destructuring that into an array, sorting it and then joining it back together. 
+
+const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+*/
