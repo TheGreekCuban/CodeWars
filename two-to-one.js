225eb1621c5996a051a10e8a1ef44ba4a6ccbef0
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-27 17:15:18 -0400

    leet speek started

--- two-to-one.js
+++ two-to-one.js
@@ -1,15 +1,3 @@
-/*
-Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters, each taken only once - coming from s1 or s2.
-
-Examples:
-a = "xyaabbbccccdefww"
-b = "xxxxyyyyabklmopq"
-longest(a, b) -> "abcdefklmopqwxy"
-
-a = "abcdefghijklmnopqrstuvwxyz"
-longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
-*/
-
 let sOne = process.argv[2]
 let sTwo = process.argv[3]
 
