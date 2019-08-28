9459ef7a49ee73fa420e96f7d5578fc55536fe6b
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-28 16:21:03 -0400

    fully functioning, need to check tests now

--- toLeetSpeak.js
+++ toLeetSpeak.js
@@ -34,13 +34,18 @@
     
       //Must destructure the object to have all properties saved to their letter variables
       let newObject = { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z  } = object
+      let newObjectKeys = Object.keys(newObject)
       let newEnglish = [ ...english.toUpperCase() ]
       let finalString = ""
+      
+      //Now that we have the input in an array, we can use map and take each letter then compare that to the object key name in the newObjectKeys array. If the values match we concat the property into an empty string.
+      newEnglish.map(letter => {
 
-      newEnglish.forEach(letter => {
-          if (letter == newObject.hasOwnProperty(letter)) {
-              finalString += newObject.letter
-          }
+        newObjectKeys.forEach(key => {
+            if(letter === key) { 
+              finalString += newObject[letter]
+            }  
+        })
       })
       console.log(`final string: ${finalString}`)
       return finalString  
