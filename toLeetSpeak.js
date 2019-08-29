0a528f159fc5916ac31e1d34f10b18cf8078f4f7
TheGreekCuban <theodorealexander92@gmaill.com>
2019-08-29 10:52:02 -0400

    fixed and completed puzzel

--- toLeetSpeak.js
+++ toLeetSpeak.js
@@ -1,4 +1,3 @@
-
   let object =  {
         A: '@',
         B: '8',
@@ -28,28 +27,43 @@
         Z: '2'
     }
 
-    english = "saffbeaofbeof"
 
     const toLeetSpeak = english => {
     
-      //Must destructure the object to have all properties saved to their letter variables
-      let newObject = { A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z  } = object
-      let newObjectKeys = Object.keys(newObject)
-      let newEnglish = [ ...english.toUpperCase() ]
-      let finalString = ""
-      
-      //Now that we have the input in an array, we can use map and take each letter then compare that to the object key name in the newObjectKeys array. If the values match we concat the property into an empty string.
-      newEnglish.map(letter => {
-
-        newObjectKeys.forEach(key => {
-            if(letter === key) { 
-              finalString += newObject[letter]
-            }  
+        //Must destructure the object to have all properties saved to their letter variables
+        let newObjectKeys = Object.keys(object)
+        let newEnglish = [ ...english.toUpperCase() ]
+        console.log(`new english ${newEnglish}`)
+        console.log(`new object keys: ${newObjectKeys}`)
+        let finalString = ''
+        
+        //Now that we have the input in an array, we can use map and take each letter then compare that to the object key name in the newObjectKeys array. If the values match we concat the property into an empty string.
+        newEnglish.forEach(letter => {
+          
+          if (letter === " ")   {
+            finalString += letter
+          }
+          newObjectKeys.map(key => {
+              if(letter === key) { 
+                finalString += object[letter]
+              } 
+          })
         })
-      })
-      console.log(`final string: ${finalString}`)
-      return finalString  
-    }
-
+        console.log(`final string: ${finalString}`)
+        return finalString  
+      }
   
-toLeetSpeak(english)
+  
+toLeetSpeak(`HELLO WORLD`)
+
+
+//BEST PRACTICES THIS USES THE REPLACE METHOD. IT ESSENTIALLY SAYS IF WE SEE ANY OF THOSE LETTERS IN THE STRING REPLACE THEM GLOBALY (G) WITH THE PROPERTY ASSOCIATED WITH THAT LETTER
+//const toLeetSpeak = s => s.replace(/[ABCEGHILOSTZ]/g, c => D[c]);
+
+const D = {
+  A: '@', B: '8', C: '(', E: '3',
+  G: '6', H: '#', I: '!', L: '1',
+  O: '0', S: '$', T: '7', Z: '2'
+};
+
+console.log(D["T"])
